1b69cdf8ccfeaa2df6971b008cfdcd155d057c96
PedroDiniz <pedro_nork@hotmail.com>
2019-06-19 12:13:48 +0000

    eslint

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,14 +1,11 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
 module.exports = {
-  storage: new multer.diskStorage({
-    destination: path.resolve(__dirname, "..", "..", "uploads"),
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
-      cb(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
-    }
-  })
+      cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    },
+  }),
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
-
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
-  "mongodb+srv://instabd:SwJMgkgnkpj8pYi4@cluster0-qdrvb.mongodb.net/test?retryWrites=true&w=majority",
+  'mongodb+srv://instabd:SwJMgkgnkpj8pYi4@cluster0-qdrvb.mongodb.net/test?retryWrites=true&w=majority',
   {
-    useNewUrlParser: true
-  }
+    useNewUrlParser: true,
+  },
 );
 
 app.use((req, res, next) => {
@@ -22,11 +21,8 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use(
-  "/files",
-  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
-);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
-app.use(require("./routes"));
+app.use(require('./routes'));
 
 server.listen(3333);
